4f00242db4fd02bfd5a109121f6c4f09e61b204c
Ryan Howard <ryanghoward@protonmail.com>
2022-02-07 14:55:42 -0800

    Save 2

--- api/server.js
+++ api/server.js
@@ -1,8 +1,25 @@
 // BUILD YOUR SERVER HERE
 
 const express = require("express");
-
+const User = require("./users/model");
 const server = express();
+
+server.get("/api/users", (req, res) => {
+  // res.json("users");
+  User.find()
+    .then((users) => {
+      // throw new Error("oh no!!!!");
+      // console.log(users);
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting users",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
 
 server.use("*", (req, res) => {
   res.status(404).json({
