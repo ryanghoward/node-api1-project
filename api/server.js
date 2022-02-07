5c6575cf657c0ebda3b8c857c0cd5a73667e1a32
Ryan Howard <ryanghoward@protonmail.com>
2022-02-07 15:07:21 -0800

    Save 3

--- api/server.js
+++ api/server.js
@@ -21,6 +21,21 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.get("/api/users:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      // console.log(user);
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting user",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
 server.use("*", (req, res) => {
   res.status(404).json({
     message: "not found",
