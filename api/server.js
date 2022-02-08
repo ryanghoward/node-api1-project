7fb8ab5104b9c34e5cabb3a5b250f3af84188db5
Ryan Howard <ryanghoward@protonmail.com>
2022-02-07 16:20:39 -0800

    Save 6 part 2: Electric Boogaloo

--- api/server.js
+++ api/server.js
@@ -6,14 +6,22 @@ const server = express();
 server.use(express.json());
 
 server.delete("/api/users/:id", async (req, res) => {
-  const possibleUser = await User.findById(req.params.id);
-  if (!possibleUser) {
-    res.status(404).json({
-      message: "The user with the specified ID does not exist",
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      const deletedUser = await User.remove(req.params.id);
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "",
+      err: err.message,
+      stack: err.stack,
     });
-  } else {
-    const deletedUser = await User.remove(req.params.id);
-    res.status(200).json(deletedUser);
   }
 });
 
